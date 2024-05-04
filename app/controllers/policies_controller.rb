dcaf5c72fcbca2561d7106fd02e51d6d8cb00ff2
Oleksandr Avoiants <shhavel@gmail.com>
2024-05-04 21:59:00 +0300

    Site policies.

---
+++ app/controllers/policies_controller.rb
@@ -0,0 +1,17 @@
+class PoliciesController < ApplicationController
+  # GET /policies/privacy-policy.html
+  def privacy_policy
+  end
+
+  # GET /policies/refund-policy.html
+  def refund_policy
+  end
+
+  # GET /policies/shipping-policy.html
+  def shipping_policy
+  end
+
+  # GET /policies/terms-of-service.html
+  def terms_of_service
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'policies/privacy-policy'
+  get 'policies/refund-policy'
+  get 'policies/shipping-policy'
+  get 'policies/terms-of-service'
   resources :products, only: %i[show], param: :handle
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
